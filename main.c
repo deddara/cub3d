8748b18a0ca77eecf18f0db56434c0056690f3ca
relasel <deddara@student.21-school.ru>
2020-07-24 20:27:55 +0300

    up to date

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int             main(void)
     int x;
     int y;
 
-    x = 5;e
+    x = 5;
     y = 5;
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
@@ -30,6 +30,6 @@ int             main(void)
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                  &img.endian);
     mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-    mlx_key_hook(vars.win, close, &vars);
+    mlx_hook(vars.win, 2, 1L<<0, close, &vars);
     mlx_loop(vars.mlx);
 }
