177478075ffa8977d27b550bfe586284f521331e
relasel <deddara@student.21-school.ru>
2020-07-28 18:58:29 +0300

    so we ea s parse added

--- map_parser/map_parser.c
+++ map_parser/map_parser.c
@@ -6,11 +6,24 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 20:53:16 by deddara           #+#    #+#             */
-/*   Updated: 2020/07/27 23:27:39 by deddara          ###   ########.fr       */
+/*   Updated: 2020/07/28 18:56:29 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map_parser.h"
+
+static int word_counter(char **str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (i != 2)
+		return (0);
+	return (1);
+}
+
 static int r_checker(char *line, t_map *map)
 {
 	int i;
@@ -32,6 +45,91 @@ static int r_checker(char *line, t_map *map)
 	return (1);
 }
 
+static int ea_checker(char *line, t_map *map)
+{
+	char **words;
+
+	map->count++;
+	words = ft_split(line, ' ');
+	if (!(word_counter(words)))
+		return (0);
+	if (words[1][0] != '.' && words[1][0] != '/')
+		return (0);
+	if(!(map->ea = ft_strdup(words[1])))
+		return (0);
+	free (*words);
+	free (words);
+	return (1);
+}	
+
+static int we_checker(char *line, t_map *map)
+{
+	char **words;
+
+	map->count++;
+	words = ft_split(line, ' ');
+	if (!(word_counter(words)))
+		return (0);
+	if (words[1][0] != '.' && words[1][0] != '/')
+		return (0);
+	if(!(map->we = ft_strdup(words[1])))
+		return (0);
+	free (*words);
+	free (words);
+	return (1);
+}	
+
+static int so_checker(char *line, t_map *map)
+{
+	char **words;
+
+	map->count++;
+	words = ft_split(line, ' ');
+	if (!(word_counter(words)))
+		return (0);
+	if (words[1][0] != '.' && words[1][0] != '/')
+		return (0);
+	if(!(map->so = ft_strdup(words[1])))
+		return (0);
+	free (*words);
+	free (words);
+	return (1);
+}	
+
+static int no_checker(char *line, t_map *map)
+{
+	char **words;
+
+	map->count++;
+	words = ft_split(line, ' ');
+	if (!(word_counter(words)))
+		return (0);
+	if (words[1][0] != '.' && words[1][0] != '/')
+		return (0);
+	if(!(map->no = ft_strdup(words[1])))
+		return (0);
+	free (*words);
+	free (words);
+	return (1);
+}	
+
+static int s_checker(char *line, t_map *map)
+{
+	char **words;
+
+	map->count++;
+	words = ft_split(line, ' ');
+	if (!(word_counter(words)))
+		return (0);
+	if (words[1][0] != '.' && words[1][0] != '/')
+		return (0);
+	if(!(map->s = ft_strdup(words[1])))
+		return (0);
+	free (*words);
+	free (words);
+	return (1);
+}	
+
 int		check_elems(char *line, t_map *map)
 {
 	int i;
@@ -40,7 +138,21 @@ int		check_elems(char *line, t_map *map)
 	if (line[0] == 'R')
 		if (!(r_checker(line, map)))
 			return (0);
-	
+	if (line[0] == 'N' && line[1] == 'O')
+		if (!(no_checker(line, map)))
+			return (0);
+	if (line[0] == 'S' && line[1] == 'O')
+		if (!(so_checker(line, map)))
+			return (0);	
+	if (line[0] == 'W' && line[1] == 'E')
+		if (!(we_checker(line, map)))
+			return (0);	
+	if (line[0] == 'E' && line[1] == 'A')
+		if (!(ea_checker(line, map)))
+			return (0);	
+	if (line[0] == 'S' && line[1] != 'O')
+		if (!(s_checker(line, map)))
+			return (0);	
 	return (1);
 }
 
