69483c3f71cbfb98dfb1b314327c996e5a14ec9e
relasel <deddara@student.21-school.ru>
2020-08-18 17:52:56 +0300

    rgb fixes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 21:22:20 by deddara           #+#    #+#             */
-/*   Updated: 2020/08/17 18:11:18 by deddara          ###   ########.fr       */
+/*   Updated: 2020/08/18 17:51:08 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,6 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
-
-//************************************************
-//ОТРИСОВКА ПОТОЛКА И ПОЛА
-
 void paint_fc(t_map *map, t_data *img)
 {
 	int y_c = 0;
@@ -37,7 +33,7 @@ void paint_fc(t_map *map, t_data *img)
 		x_c = 0;
 		while (x_c < map->x)
 		{
-			my_mlx_pixel_put(img, x_c, y_c, 0x00007E);
+			my_mlx_pixel_put(img, x_c, y_c, map->c_rgb);
 			x_c++;
 		}
 		y_c++;
@@ -47,94 +43,12 @@ void paint_fc(t_map *map, t_data *img)
 		x_c = 0;
 		while (x_c < map->x)
 		{
-			my_mlx_pixel_put(img, x_c, y_c, 0x343A41);
+			my_mlx_pixel_put(img, x_c, y_c, map->f_rgb);
 			x_c++;
 		}
 		y_c++;
 	}
 }
-
-//ОТРИСОВКА ПОТОЛКА И ПОЛА
-//************************************************
-// static int skipper(t_map *map, int x, int y)
-// {
-// 	if (map->map[y][x] == ' ' || map->map[y][x] == '0' || map->map[y][x] == '2' || 
-// 		map->map[y][x] == 'N' || map->map[y][x] == 'S' || map->map[y][x] == 'W'
-// 		|| map->map[y][x] == 'E')
-// 		return (1);
-// 	return (0);
-// }
-
-// static void draw_rect(int x, int y, t_map *map, t_data *img)
-// {
-// 	int rect_x;
-// 	int rect_y;
-// 	int i, j;
-
-// 	i = 0;
-// 	j = 0;
-	
-// 	rect_x = x * (map->x / map->x_count / 3);
-// 	rect_y = y * (map->y / map->y_count / 3);
-	
-// 	while (i < (map->x / map->x_count / 3) - 1)
-// 	{
-// 		j = 0;
-// 	 	while (j < (map->y / map->y_count / 3) - 1)
-// 		{
-// 			my_mlx_pixel_put(img, rect_x + i, rect_y + j, 0xFFFF41);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
-
-// static void draw_player(int x, int y, t_map *map, t_data *img)
-// {
-// 	int rect_x;
-// 	int rect_y;
-// 	int i, j;
-
-// 	i = 0;
-// 	j = 0;
-	
-// 	rect_x = x * (map->x / map->x_count / 3) + ((map->x / map->x_count / 3) / 2);
-// 	rect_y = y * (map->y / map->y_count / 3) + ((map->y / map->y_count / 3) / 2);
-	
-// 	while (i < 5)
-// 	{
-// 		j = 0;
-// 	 	while (j < 5)
-// 		{
-// 			my_mlx_pixel_put(img, rect_x + i, rect_y + j, 0xFF0000);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
-
-// static void paint_map(t_map *map, t_data *img)
-// {
-// 	int x = 0;
-// 	int y = 0;
-
-// 	while (map->map[y])
-// 	{
-// 		x = 0;
-// 		while (map->map[y][x])
-// 		{
-// 			if (skipper(map, x, y))
-// 			{
-// 				x++;
-// 				continue ;
-// 			}
-// 			draw_rect(x, y, map, img);
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// }
-
 static int _init(t_raycast *ray, t_map *map)
 {
 	int i;
@@ -164,8 +78,6 @@ static void keys_init (t_raycast *ray)
 
 int             main(void)
 {
-	// paint_map(&map, &img);
-	// draw_player(map.x_player, map.y_player, &map, &img);
     t_data  img;
     t_vars vars;
     t_map map;
