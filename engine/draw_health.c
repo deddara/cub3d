acf8e3e3df2ab69276c6d0a0938512d183d7e91a
relasel <deddara@student.21-school.ru>
2020-08-28 15:42:28 +0300

    map added

--- engine/draw_health.c
+++ engine/draw_health.c
@@ -6,27 +6,28 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/27 13:35:57 by deddara           #+#    #+#             */
-/*   Updated: 2020/08/27 18:05:02 by deddara          ###   ########.fr       */
+/*   Updated: 2020/08/28 15:41:00 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "engine.h"
 #include "main.h"
 
-int		draw_health(t_raycast *ray)
+int			draw_health(t_raycast *ray)
 {
 	int y;
 	int x;
 	int pixel;
-	
+
 	y = 0;
 	while (y < ray->map->y / 7)
-	{	
+	{
 		x = 0;
 		while (x < ray->map->x / 5)
 		{
-			pixel = getpixelcolor(&ray->health, (int)((double)((x / (double)(ray->map->x / 5)) * ray->health.width)),\
-			(int)((double)((y / (double)(ray->map->y / 7)) * ray->health.height)));
+			pixel = getpixelcolor(&ray->health, (int)((double)\
+			((x / (double)(ray->map->x / 5)) * ray->health.width)), (int)\
+			((double)((y / (double)(ray->map->y / 7)) * ray->health.height)));
 			if ((pixel & 0x00FFFFFF) != 0)
 				my_mlx_pixel_put(ray->img, ray->map->x - x, y, pixel);
 			x++;
@@ -36,74 +37,82 @@ int		draw_health(t_raycast *ray)
 	return (1);
 }
 
-static int skipper2(t_map *map, int x, int y)
+static void	draw_rect(int x, int y, t_map *map, t_data *img)
 {
-	if (map->map[y][x] == ' ' || map->map[y][x] == '0' || map->map[y][x] == '2' || 
-		map->map[y][x] == 'N' || map->map[y][x] == 'S' || map->map[y][x] == 'W'
-		|| map->map[y][x] == 'E')
-		return (1);
-	return (0);
-}
-
-static void draw_rect(int x, int y, t_map *map, t_data *img)
-{
-	int rect_x;
-	int rect_y;
-	int i, j;
+	int	rect_x;
+	int	rect_y;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	
 	rect_x = x * (map->x / map->x_count / 3);
 	rect_y = y * (map->y / map->y_count / 3);
-	
 	while (i < (map->x / map->x_count / 3) - 1)
 	{
 		j = 0;
-	 	while (j < (map->y / map->y_count / 3) - 1)
+		while (j < (map->y / map->y_count / 3) - 1)
 		{
-			my_mlx_pixel_put(img, rect_x + i, rect_y + j, 0xFFFF41);
+			my_mlx_pixel_put(img, rect_x + i, rect_y + j, 0x01D4F8);
 			j++;
 		}
 		i++;
 	}
 }
 
-static void draw_player(double x, double y, t_map *map, t_data *img)
+static void	draw_dir(t_raycast *ray, double rect_x, double rect_y)
 {
-	double rect_x;
-	double rect_y;
-	int i, j;
+	int k;
+
+	k = 0;
+	rect_x += 2;
+	rect_y += 2;
+	while (k < 10)
+	{
+		my_mlx_pixel_put(ray->img, rect_x, rect_y, 0xFF0000);
+		rect_x += ray->ray_dir_x;
+		rect_y += ray->ray_dir_y;
+		k++;
+	}
+}
+
+static void	draw_player(t_raycast *ray)
+{
+	double	rect_x;
+	double	rect_y;
+	int		i;
+	int		j;
 
 	i = 0;
-	j = 0;
-	
-	rect_x = (x - 0.6) * (map->x / map->x_count / 3) + ((map->x / map->x_count / 3) / 2);
-	rect_y = (y - 0.6) * (map->y / map->y_count / 3) + ((map->y / map->y_count / 3) / 2);
-	
+	rect_x = (ray->player_x - 0.6) * (ray->map->x / ray->map->x_count / 3) \
+	+ ((ray->map->x / ray->map->x_count / 3) / 2);
+	rect_y = (ray->player_y - 0.6) * (ray->map->y / ray->map->y_count / 3) \
+	+ ((ray->map->y / ray->map->y_count / 3) / 2);
 	while (i < 5)
 	{
 		j = 0;
-	 	while (j < 5)
+		while (j < 5)
 		{
-			my_mlx_pixel_put(img, rect_x + i, rect_y + j, 0xFF0000);
+			my_mlx_pixel_put(ray->img, rect_x + i, rect_y + j, 0xFF0000);
 			j++;
 		}
 		i++;
 	}
+	draw_dir(ray, rect_x, rect_y);
 }
 
-void paint_mini_map(t_raycast *ray)
+void		paint_mini_map(t_raycast *ray)
 {
-	int x = 0;
-	int y = 0;
+	int x;
+	int y;
 
+	y = 0;
 	while (ray->map->map[y])
 	{
 		x = 0;
 		while (ray->map->map[y][x])
 		{
-			if (skipper2(ray->map, x, y))
+			if (skipper(ray->map, y, x))
 			{
 				x++;
 				continue ;
@@ -113,5 +122,5 @@ void paint_mini_map(t_raycast *ray)
 		}
 		y++;
 	}
-	draw_player(ray->player_x, ray->player_y, ray->map, ray->img);
+	draw_player(ray);
 }
